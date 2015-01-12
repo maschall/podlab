f79d80c98ac947a709d5ebf8ceec4de6cb98f7bd
Mark Schall <mark@detroitlabs.com>
2015-01-12 10:49:08 -0500

    adding itunes search class

--- iTunesSearch/iTunesSearch.swift
+++ iTunesSearch/iTunesSearch.swift
@@ -7,7 +7,21 @@
 //
 
 import UIKit
+import Alamofire
 
 public class iTunesSearch: NSObject {
-   
+    
+    public class func search( term: String, callback: ( [[String: AnyObject]]?, NSError? ) -> Void ) {
+        Alamofire.request(.GET, "http://itunes.apple.com/search?limit=200&media=podcast&term=\(term)")
+            .responseJSON { (request, response, json, error) -> Void in
+                if let error = error {
+                    callback( nil, error )
+                } else if let json = json as? [String: AnyObject] {
+                    if let results = json["results"] as? [[String: AnyObject]] {
+                        callback( results, nil )
+                    }
+                }
+        }
+        
+    }
 }
